5f3f419cd521bcc7a3bb228be973f2dc7a121187
Mike <fuchun297@gmail.com>
2024-10-07 19:52:58 -0400

    first commit

---
+++ src/main/java/com/omni/code/TrendMart.java
@@ -0,0 +1,13 @@
+package com.omni.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendMart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendMart.class, args);
+	}
+
+}
