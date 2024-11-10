eed72a9aefe5cce7edb922ea49bc71989ecfa1d4
Mike <fuchun297@gmail.com>
2024-11-10 08:04:43 -0500

    update for fixing error

--- src/main/java/com/omni/code/controller/CheckoutController.java
+++ src/main/java/com/omni/code/controller/CheckoutController.java
@@ -1,6 +1,5 @@
 package com.omni.code.controller;
 
-import com.omni.code.entity.Order;
 import com.omni.code.request.CheckoutRequest;
 import com.omni.code.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
